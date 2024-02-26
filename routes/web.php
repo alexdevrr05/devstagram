7fd44608f8c1072a5465a6d5b31ef364ec6ee61c
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-02-26 17:26:43 -0600

    Refactor web routes to use RegisterController

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,16 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// closure
 Route::get('/', function () {
     return view('principal');
 });
 
-Route::get('/crear-cuenta', function () {
-    return view('auth.register');
-});
+/**
+ * una convención común nombrar el método
+ *  que devuelve la vista principal de un recurso como "index"
+ */
+// controlador
+Route::get('/crear-cuenta', [RegisterController::class, 'index']);

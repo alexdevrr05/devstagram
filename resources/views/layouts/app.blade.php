e7759b0d4d9c2e3bfe43b4b0adc6495cbf2e88ac
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-02-26 10:34:36 -0600

    Add Tailwind CSS and PostCSS dependencies

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,21 +4,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
+    <title>DevStagram - @yield('titulo')</title>
+    @vite('resources/css/app.css')
 </head>
 
 <body>
-    <nav>
-        <a href="/">Principal</a>
-        <a href="/nosotros">Nosotros</a>
-    </nav>
+    <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
 
-    <h1>@yield('titulo')</h1>
     <hr />
 
     @yield('contenido')
-
 </body>
 
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    // Todo lo que esté dentro de resources
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};

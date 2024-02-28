d61421d0774967a119391a25a8de24ede3c80c46
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-02-28 11:56:45 -0600

    Se agregan mas validaciones para el campo de password

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -27,9 +27,11 @@ class RegisterController extends Controller
             'name' => 'required|max:30',
             'username' => 'required|unique:users|min:3|max:20',
             'email' => "required|unique:users|email|max:60",
-            'password' => "required",
+            'password' => "required|confirmed|min:6",
             'password_confirmation' => "",
         ]);
+
+        dd('Creando usuario...');
 
         // otra sintáxis
         // $this->validate($request, [

77558155c2ba7b98046711c68b48bccc0523c30d
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-03-03 09:33:04 -0600

    feat: proteger la ruta de 'post' para que la vean solo usuarios autenticados

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function __construct()
+    {
+        // protege la ruta (solo se muestra si hay una sesion iniciada)
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('dashboard');

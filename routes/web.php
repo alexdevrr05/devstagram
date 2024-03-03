2edccbbda86a6adf0a3c61b3e7fadbf2f4315f29
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-03-03 09:37:58 -0600

    feat: agregar ruta hacia '/login' y crear vista de login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -14,7 +15,9 @@ Route::get('/', function () {
  *  que devuelve la vista principal de un recurso como "index"
  */
 // controlador
-Route::get('/crear-cuenta', [RegisterController::class, 'index'])->name('register');
-Route::post('/crear-cuenta', [RegisterController::class, 'store']);
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
 
 Route::get('/muro', [PostController::class, 'index'])->name('posts.index');

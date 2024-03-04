b7f917add426a0bd730bf76a9bbf5e79fbbf4911
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-03-04 16:47:43 -0600

    chore: instalar dependencia de dropzone

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import Dropzone from "dropzone";
+
+/**
+ * Por defecto dropzone va a buscar un elemento que tenga la
+ * clase de dropzone, pero nosotros queremos darle el comportamiento
+ */
+Dropzone.autoDiscover = false;

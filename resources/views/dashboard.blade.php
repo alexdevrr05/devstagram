592a27b3fa5e9cd6a3707ee4439037f3625169b2
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-03-04 09:44:24 -0600

    feat: agregar informacion en el perfil (muro)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,30 @@
 @extends('layouts.app')
 
 @section('titulo')
-Tu cuenta
+Perfil: {{ $user->username }}
 @endsection
 
 @section('contenido')
 <div class="flex justify-center">
-    <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
+    <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
 
-        <div class="md:w-8/12 lg:w-6/12 px-5">
+        <div class="w-8/12 lg:w-6/12 px-5">
             <img src="{{ asset('imgs/usuario.svg') }}" alt="Imagen de usuario">
         </div>
-        <div class="md:w-8/12 lg:w-6/12 px-5">
+        <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center py-10 md:justify-center md:items-start md:py-0">
             <p class="text-gray-700 text-xl">{{ $user->username }}</p>
+            <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
+                0
+                <span class="font-normal"> Seguidores</span>
+            </p>
+            <p class="text-gray-800 text-sm mb-3 font-bold">
+                0
+                <span class="font-normal"> Siguiendo</span>
+            </p>
+            <p class="text-gray-800 text-sm mb-3 font-bold">
+                0
+                <span class="font-normal"> Posts</span>
+            </p>
         </div>
 
     </div>

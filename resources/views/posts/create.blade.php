a4cedf4340a771763e0c6eb6440bcc2894892139
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-03-04 11:19:11 -0600

    style: finalizar el formulario de posts (publicaciones)

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,17 +13,30 @@ Crea una nueva publicación
         <form action="{{ route('register') }}" method="POST">
             @csrf
             <div class="mb-5">
-                <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">Nombre</label>
-                {{-- @error funciona como un ternario de react @enderror --}}
-                <input type="text" id="name" name="name" placeholder="Tu nombre"
+                <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">titulo</label>
+                <input type="text" id="titulo" name="titulo" placeholder="Título de la publicación"
                     class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
-                    value="{{ old('name') }}">
+                    value="{{ old('titulo') }}">
 
-                {{-- SI HAY un error al enviar el campo mostrar mensaje de error VARIABLE --}}
-                @error('name')
+                @error('titulo')
                 <p class="bg-red-500 text-white my-2 rounded-lg texts-sm p-2 text-center">{{ $message }}</p>
                 @enderror
             </div>
+
+            <div class="mb-5">
+                <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">Descripción</label>
+                <textarea id="descripcion" name="descripcion" placeholder="Descripción de la publicación"
+                    class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
+                    value="{{ old('descrpcion') }}">
+                </textarea>
+
+                @error('titulo')
+                <p class="bg-red-500 text-white my-2 rounded-lg texts-sm p-2 text-center">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <input type="submit" value="Publicar"
+                class="cursor-pointer bg-sky-600 hover:bg-sky-700 transition-colors uppercase font-bold text-white w-full p-3 rounded-lg">
         </form>
     </div>
 </div>

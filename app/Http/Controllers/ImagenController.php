362ba1f1258b54de03b8868a37de8bdb3cd15f0c
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-03-05 09:25:50 -0600

    chore: instalar la dependencia 'intervention/image'

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\ImageManager;
 
 class ImagenController extends Controller
 {
     public function store(Request $request)
     {
-        // $input = $request->all();
         $imagen = $request->file('file');
-        return response()->json(['imagen' => $imagen->extension()]);
+
+        // Nombramos la imagen con un uudi y agregamos su formato/extension
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+        // Leemos la imagen
+        $imagenServidor = ImageManager::imagick()->read($imagen);
+        // Cambiamos las dimensiones de la imagen
+        $imagenServidor->resize(1000, 1000);
+        // Creamos el path donde se guardará la imagen
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        // Guardamos la imagen
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }

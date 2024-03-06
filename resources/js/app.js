31b5efc290d7713d88aa2c711d4c22a1594bc179
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-03-06 07:47:40 -0600

    feat: agregar persistencia en la imagen del dropzone y resetear al value si se borra manualmente

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,9 +13,40 @@ const dropzone = new Dropzone("#dropzone", {
     dictRemoveFile: "Borrar archivo",
     maxFiles: 1,
     uploadMultiple: false,
+
+    /**
+     * Si el usuario manda el formulario y la imagen es válida,
+     *  pero está mal algo en la data entonces hace persistente la imagen */
+    init: function () {
+        // Si el value de la imagen tiene valor
+        if (document.querySelector('[name="imagen"]').value.trim()) {
+            const imagenPublicada = {};
+            // por colocar un size
+            imagenPublicada.size = 1234;
+            imagenPublicada.name =
+                document.querySelector('[name="imagen"]').value;
+
+            this.options.addedfile.call(this, imagenPublicada);
+            this.options.thumbnail.call(
+                this,
+                imagenPublicada,
+                `/uploads/${imagenPublicada.name}`
+            );
+
+            imagenPublicada.previewElement.classList.add(
+                "dz-success",
+                "dz-complete"
+            );
+        }
+    },
 });
 
-dropzone.on("success", (file, response) => {
+dropzone.on("success", function (file, response) {
     // se toma response.imagen porque asi retornamos el json (en ImagenController)
     document.querySelector('[name="imagen"]').value = response.imagen;
+});
+
+dropzone.on("removedfile", function () {
+    // reseteo del valor (si se elimina la imagen)
+    document.querySelector('[name="imagen"]').value = "";
 });

8c004339ca0749ecfbd628ed88c0b789777f158b
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-03-07 08:47:39 -0600

    style: crear el formulario para agregar comentarios

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,8 +22,26 @@ Post: {{ $post->titulo }}
             <p class="mt-5">{{ $post->descripcion }}</p>
         </div>
     </div>
-    <div class="md:w-1/2">
-        2
+    <div class="md:w-1/2 p-5">
+        <div class="shadow bg-white p-5 mb-5">
+            <p class="text-xl font-bold text-center mb-4">Agrega un nuevo comentario</p>
+            <form action="">
+                <div class="mb-5">
+                    <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">Añade un
+                        comentario</label>
+                    <textarea id="comentario" name="comentario" placeholder="Escribe tu comentario" class="border p-3 w-full rounded-lg @error('name') border-red-500 
+                    @enderror"></textarea>
+
+                    @error('comentario')
+                    <p class="bg-red-500 text-white my-2 rounded-lg texts-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <input type="submit" value="Comentar"
+                    class="cursor-pointer bg-sky-600 hover:bg-sky-700 transition-colors uppercase font-bold text-white w-full p-3 rounded-lg">
+
+            </form>
+        </div>
     </div>
 </div>
 @endsection

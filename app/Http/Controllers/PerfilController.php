a4ae3cb743d53c65c7a69e8d438a6b67d0a5fb7f
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-03-13 08:36:18 -0600

    Actualizar el controlador de Perfil para guardar la información actualizada

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Intervention\Image\ImageManager;
 
 class PerfilController extends Controller
 {
@@ -31,5 +32,25 @@ class PerfilController extends Controller
             // cuando hay mas de 3 reglas de validacion es recomendable usar un arreglo
             'username' => ['required', 'unique:users,username,'.auth()->user()->id, 'min:3', 'max:20', 'not_in:twitter,editar-perfil'],
         ]);
+
+        if($request->imagen) {
+            $imagen = $request->file('imagen');
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+            $imagenServidor = ImageManager::imagick()->read($imagen);
+            $imagenServidor->resize(1000, 1000);
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+
+        // Guardar cambios
+        $usuario = User::find(auth()->user()->id);
+
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->save();
+
+        // Redireccionar al perfil del usuario
+        // $usuario->username = "ultima copia" (por si actualiza el username)
+        return redirect()->route('posts.index', $usuario->username);
     }
 }
